e5e51c5c5b6e7b41b1142ab419af4b7dfcc69da7
void <49158033+Narick09@users.noreply.github.com>
2020-12-13 21:43:30 +0700

    Update indicator.py

--- indicator.py
+++ indicator.py
@@ -1,62 +1,60 @@
 #!/usr/bin/python3
-import datetime
-import pcapy
+
+from scapy.all import sniff
 import threading
 from matplotlib import pyplot
 from matplotlib import animation
-packet_count = 0
-data_count = 0
-last_timebin = 0
+import sys
+
+### Reading arguments
+if len(sys.argv) > 2:
+    print('Usage:', sys.argv[0], '[filter]')
+    exit()
+if len(sys.argv) == 2:
+    filt = sys.argv[1]
+else:
+    print('Enter valid pcap filter:')
+    filt = input()
+###
+
+### Capturing packets
+time_points = [0]
+data = [0]
 
 
-class PcapThread(threading.Thread):
-    def __init__(self, cap):#constructor
-        threading.Thread.__init__(self)
-        self.cap = cap
-
-    def run(self):
-        global packet_count, data_count
-        while True:
-            something, packet = self.cap.next()
-            packet_count += 1
-            data_count += len(packet)
+def handlePacket(packet):
+    global data
+    data[-1] += len(bytes(packet))
 
 
-def plot_cont():
-    start_time = datetime.datetime.now()
-    packets = [0]
-    data = [0]
-    fig = pyplot.figure()
-    plot_packets = fig.add_subplot(1, 2, 1)
-    plot_data = fig.add_subplot(1, 2, 2)
-    fig.canvas.set_window_title("Network indicator")
+capture_thread = threading.Thread(target=sniff, kwargs={'prn': handlePacket, 'filter': filt})
+capture_thread.start()
+print('Started capturing...')
+###
 
-    def update(i):
-        global packet_count, data_count, last_timebin
-        time_p = datetime.datetime.now()
-        if (time_p - start_time).seconds > last_timebin:
-            last_timebin = (time_p - start_time).seconds
-            packets.append(packet_count)
-            data.append(data_count)
-            data_count = 0
-            packet_count = 0
-        if len(packets) > 50:
-            del packets[0]
-            del data[0]
-        x = range(len(packets))
-
-        plot_packets.clear()
-        plot_packets.set_title("Packets")
-        plot_packets.plot(x, packets)
-
-        plot_data.clear()
-        plot_data.set_title("Data")
-        plot_data.plot(x, data)
-    a = animation.FuncAnimation(fig, update, repeat=False)
-    pyplot.show()
+### Drawing the plot
+time_interval = 0.5  # seconds
+time_width = 15  # seconds
 
 
-cap = pcapy.open_live("wlp2s0", 4163, 1, 50)
-thread1 = PcapThread(cap)
-thread1.start()
-plot_cont()
+def animate(i):
+    while len(data) * time_interval > time_width:
+        data.pop(0)
+        time_points.pop(0)
+
+    plot.clear()
+    plot.set_title('Network activity')
+    plot.set_xlabel('Seconds, s')
+    plot.set_ylabel('Data, bytes')
+    plot.plot(time_points, data)
+
+    data.append(0)
+    time_points.append(time_points[-1] + time_interval)
+
+
+figure = pyplot.figure()
+figure.canvas.set_window_title('Network activity')
+plot = figure.add_subplot(111)
+a = animation.FuncAnimation(figure, animate, interval=time_interval * 1000)
+pyplot.show()
+###
