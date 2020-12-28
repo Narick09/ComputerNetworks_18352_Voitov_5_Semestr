c10356322cc98667b8408607c176116a5008f592
void <49158033+Narick09@users.noreply.github.com>
2020-12-28 10:31:56 +0700

    Update server.py

--- tcp-chat/server.py
+++ tcp-chat/server.py
@@ -1,69 +1,50 @@
-import threading
 import socket
+import threading
 
-# Connection Data
-host = '127.0.0.1'  # local host
-port = 7777
+# Choosing Nickname
+nickname = input("Choose your nickname: ")
 
-# Starting Server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      #af_inet - доступные для подключения, стрим - из сокета можно поток сделать - tcp-protocol
-server.bind((host, port))
-server.listen()
-
-# Lists For Clients and Their Nicknames
-clients = []
-nicknames = []
+# Connecting To Server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 7777))
 
 
-# Sending Messages To All Connected Clients
-def broadcast(message):
-    for client in clients:
-        client.send(message)
-
-
-# Handling Messages From Clients
-def handle(client):
+# Listening to Server and Sending Nickname
+def receive():
+    global stop_thread
+    stop_thread = False
     while True:
+        if stop_thread:
+            break
         try:
-            # Broadcasting Messages
-            message = client.recv(1024) #receive mesege
-            broadcast(message)          #to broadcast
-        except:
-            # Removing And Closing Clients
-            index = clients.index(client)
-            clients.remove(client)
+            # Receive Message From Server
+            # If 'NICK' Send Nickname
+            message = client.recv(1024).decode('ascii') #receive usual message
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))   #send nickname by pipe
+            else:
+                print(message)
+        except:         #if server connection не удалось установить - если не можем получить сообщ от серва
+            # Close Connection When Error
+            print("An error occurred!")
             client.close()
-            nickname = nicknames[index]
-            broadcast('{} left!'.format(nickname).encode('ascii'))
-            nicknames.remove(nickname)
+            stop_thread = True
             break
 
 
-# Receiving / Listening Function
-def receive():
+# Sending Messages To Server
+def write():
     while True:
-        # Accept Connection
-        client, address = server.accept()                       #sleep,wait 
-        print("Connected with {}".format(str(address)))
-
-        # Request Nickname
-        client.send('NICK'.encode('ascii'))                     #запрос ника клиента
-        nickname = client.recv(10240).decode('ascii')           #client return nick & decode it
-
-        # Store Nickname and Client
-        nicknames.append(nickname)                              #save nick at list
-        clients.append(client)
-
-        # Print And Broadcast Nickname
-        print("Nickname is {}".format(nickname))                #message to server
-        broadcast("{} joined!".format(nickname).encode('ascii'))
-        client.send('Connected to server!'.encode('ascii'))     #message for cl
-
-        # Start Handling Thread For Client - start thread
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
+        if stop_thread:
+            break
+        message = '{}: {}'.format(nickname, input(''))  #в фигурные собки - то,что внутри формата 
+        client.send(message.encode('ascii'))            #by broadcast
 
 
-print('Server is listening...')
-receive()       #start func
+# Starting Thread for Listening
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()  #пишем
 
+# Starting Thread for Writing
+write_thread = threading.Thread(target=write)
+write_thread.start()    #слушаем
